fcaab66d5eeb62a27247c3995253f0f6098ba350
Chris309283 <Christopher.holmelund@gmail.com>
2024-02-18 16:50:15 +0100

    expanded on game from toturial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,23 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public float jumpStrength = 5f;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+    private bool isGrounded;
 
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
+    private bool jump = false;
+    
+    private Vector3 startPosition; 
+    public float deathHeight = -10f;
 
     void Start()
     {
@@ -21,6 +31,7 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText();
         winTextObject.SetActive(false);
+        startPosition = transform.position;
     }
 
     void OnMove(InputValue movementValue)
@@ -28,6 +39,14 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            jump = true;
+        }
     }
 
     void SetCountText()
@@ -42,8 +61,20 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+
+        if (jump && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            jump = false;
+        }
+        
+        if (transform.position.y < deathHeight)
+        {
+            Respawn();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -54,5 +85,18 @@ public class PlayerController : MonoBehaviour
             count++;
             SetCountText();
         }
+        
+        else if (other.gameObject.CompareTag("DeathWalls")) 
+        {
+            Respawn(); 
+        }
+    }
+    
+    void Respawn()
+    {
+        count = 0;
+        SetCountText();
+        rb.velocity = Vector3.zero; 
+        transform.position = startPosition; 
     }
 }
